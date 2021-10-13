390833676f34010bb6a27a3154b702f235a47b18
Tanapol Dangsakul <62011301@kmitl.ac.th>
2021-10-13 16:11:49 +0700

    New Update 1
    Add canvas for score and time
    Add game controller to control time
    Add prefab for canvas and controller

---
+++ MiniAssignment/Assets/Scripts/GameControl.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    // Score and time
+    public int score;
+    public float time;
+
+    // Text Canvas
+    public TMPro.TextMeshProUGUI timeText;
+    public TMPro.TextMeshProUGUI scoreText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // While the game is running
+        if (time >= 0)
+        {
+            // Countdown timer
+            time -= Time.deltaTime;
+            // Update Canvas
+            timeText.text = "Time Remaining: " + Math.Round(time);
+            scoreText.text = "Score: " + score;
+        }
+        else
+        {
+            // Game over, stop the time
+            Time.timeScale = 0;
+        }
+    }
+}
