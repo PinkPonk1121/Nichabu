bc28f4d52647efaaad583904cafda20e269b1f29
proudkittapa <proudy2013@gmail.com>
2021-10-21 15:46:36 +0700

    add comment

--- MiniAssignment/Assets/Scripts/Environment.cs
+++ MiniAssignment/Assets/Scripts/Environment.cs
@@ -31,6 +31,7 @@ public class Environment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
+        //get position of the screen
         Vector3 topRight = Camera.current.ScreenToWorldPoint(new Vector3(Camera.current.pixelWidth, Camera.current.pixelHeight, 1.5f));
         Vector3 bottomLeft = Camera.current.ScreenToWorldPoint(new Vector3(0, 0, 1.5f));
         Vector3 screenCenter = Camera.current.ScreenToWorldPoint(new Vector3(Camera.current.pixelWidth/2, Camera.current.pixelHeight/2, 1.5f));
@@ -40,30 +41,26 @@ public class Environment : MonoBehaviour
 
         while (count < numOfItems)
         { 
-            // create new items as the game start
-            // if (count % 3 == 0) // 0 && 1
-            // {
-            //     Spawner(count, meatPrefab);
-            // }
-            // else if (count % 3 == 1) // 2
-            // {
-            //     Spawner(count, veggiePrefab);              
-            // }
-            itemArray[count] = Instantiate(meatPrefab, new Vector3(screenCenter.x,screenCenter.y,1.3f), Quaternion.Euler(0f, 90f, 270f));
-            // itemSpawnSound.Play();
-            
+            //instantiate meat using the position above
+            itemArray[count] = Instantiate(meatPrefab, new Vector3(screenCenter.x,screenCenter.y,1.3f), Quaternion.Euler(0f, 90f, 270f));            
             count++;
         }
 
+        //loop through all meats but we only have 1 meat for now
         for (int i = 0; i < numOfItems; i++)
         {            
             var foodBody = itemArray[i].GetComponent<Rigidbody>();
+            //add force to meat so the meat move downward
             foodBody.AddForce(new Vector3(0f, -0.06f, 0f), ForceMode.Impulse);
+            //check if the meat reach the bottom screen
             if (itemArray[i].transform.position.y < bottomLeft.y)
             {
+                //make the meat invisible
                 itemArray[i].SetActive(false);
+                //reset the velocity to 0
                 foodBody.velocity = Vector3.zero;
                 
+                //get depth of the pot
                 Vector3 potPos = new Vector3(0f,0f,0f);
                 Vector3 camPos = Camera.current.transform.position;
                 foreach (ARFace face in faceManager.trackables)
@@ -85,17 +82,13 @@ public class Environment : MonoBehaviour
                 Vector3 camToPlane = Vector3.Project(camToPot, camFor);
                 float depth = camToPlane.magnitude;
                 y.text = depth.ToString();
+
+                //create new position to spawn the meat after the meat reach the bottom
                 Vector3 spawnPosition = Camera.current.ScreenToWorldPoint(new Vector3(Random.Range(0, Camera.current.pixelWidth), Camera.current.pixelHeight, depth));
                 itemArray[i].transform.position = spawnPosition;  
+                //change the rotation according to the rotation of the camera
                 itemArray[i].transform.rotation = Quaternion.Euler(0f, 90f+Camera.main.transform.localEulerAngles.y, 270f);
-                // x.text = "face pos" + face.transform.position.ToString();
-                // x.text = "Rotation" + Camera.main.transform.rotation.ToString();
-                // y.text = "Rotation2" + Camera.main.transform.localEulerAngles.ToString();
-                // y.text = "Meat pos" + itemArray[0].transform.position.ToString();
-                // x.text = "Pot position" + Manager.facePrefab.transform.position.ToString();
-                // z.text = "Pot position" + test.facePrefab.ToString();
-                // GameControl.score++;
-
+                //make the meat visible
                 itemArray[i].SetActive(true);
             }
 
@@ -104,30 +97,22 @@ public class Environment : MonoBehaviour
         }
     }
 
-    // private void Spawner(int count, GameObject food)
-    // {
-    //     yield return new WaitForSeconds(Random.Range(1f, 2f));
-
-    //     itemArray[count] = Instantiate(food, new Vector3(screenCenter.x,screenCenter.y,1.3f), Quaternion.Euler(0f, 90f, 270f));
-
-    // }
-
-    // public static void FoodRespawn(GameObject f){
-    //     StartCoroutine(RespawnFood(f));
-    // }
-
+    //coroutines for respawn the food
     public static IEnumerator RespawnFood(GameObject food, float depth) {
-        
+        //set the food to invisible
         food.SetActive(false);
+        //set the velocity to zero
         var foodBody = food.GetComponent<Rigidbody>();
         foodBody.velocity = Vector3.zero;
-
+        //wait for 1-2 second 
         yield return new WaitForSeconds(Random.Range(1f, 2f));
 
+        //set the new position to spawn food
         Vector3 spawnPosition = Camera.current.ScreenToWorldPoint(new Vector3(Random.Range(0, Camera.current.pixelWidth), Camera.current.pixelHeight, depth));
-        // foodSpawn.Play();
-        food.transform.position = spawnPosition;  
+        food.transform.position = spawnPosition; 
+        //make the food visible 
         food.SetActive(true);
+        //add force to the food
         foodBody.AddForce(new Vector3(0f, -0.06f, 0f), ForceMode.Impulse);
     }
 }
--- MiniAssignment/Assets/Scripts/Pot.cs
+++ MiniAssignment/Assets/Scripts/Pot.cs
@@ -4,33 +4,29 @@ using UnityEngine;
 
 public class Pot : MonoBehaviour
 {
-    // public Environment env = gameObject.GetComponent<Environment>();
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-
-    // Update is called once per frame
     void Update()
     {
-        // Environment.x.text = "test pot.cs";
-        // GameControl.score++;
     }
     private void OnCollisionEnter(Collision collision){
-        // GameControl.score++;
+        //get the depth of the pot        
         Vector3 camPos = Camera.current.transform.position;
         Vector3 potPos = this.transform.position;
         Vector3 camToPot = potPos - camPos;
         Vector3 camFor = Camera.current.transform.forward;
         Vector3 camToPlane = Vector3.Project(camToPot, camFor);
         float depth = camToPlane.magnitude;
+
+        //if the pot collide with object with "Meat" tag
         if (collision.gameObject.tag == "Meat"){
-            // eatSound.Play()
+            //increase the score
             GameControl.score++;
-            // Debug.Log(pts);
-            // env.FoodRespawn(collision.gameObject);
+            //start to respawn food 
             StartCoroutine(Environment.RespawnFood(collision.gameObject, depth));
         }
     }
